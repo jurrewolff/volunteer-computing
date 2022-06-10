12ce8b2df01fa7cecd0b111105824a45c84ff328
Jeffrey Kragten <Jeffreykragten@live.nl>
2022-06-10 14:24:43 +0200

    can now insert users and check credentials against database

--- mysql_script.py
+++ mysql_script.py
@@ -1,17 +1,61 @@
-import mysql.connector
+import mysql.connector as connector
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="admin",
-  database="app"
-)
+def connection():
 
-mycursor = mydb.cursor()
+    config = {
+        "user": "root",
+        "password": "admin",
+        "host": "localhost",
+        "port": 3306,
+        "database": "app"
+    }
+    try:
+        c = connector.connect(**config)
+        return c
+    except:
+        print("connection error")
+        exit(1)
 
-mycursor.execute("SELECT * FROM User")
+def test(): # error method
+    cur= connection().cursor()
+    cur.execute("SELECT * FROM User")
+    print(cur.fetchone())
 
-myresult = mycursor.fetchall()
+def test1(): #no error method
+    cn = connection()
+    cur = cn.cursor()
+    cur.execute("SELECT * FROM User")
+    res = cur.fetchall()
+    for x in res:
+        print(x)
 
-for x in myresult:
-  print(x)
+
+
+
+def insert_user(val):
+    if check_user_exists(val[0], val[1]) == None:
+        print('Added new user')
+        cn = connection()
+        cur = cn.cursor()
+        sql = "INSERT INTO User VALUES (%s, %s, %s, %s, %s, %s)"
+        cur.execute(sql, val)
+        cn.commit()
+    print('UserID or UserName not unique')
+
+
+def check_user_exists(id, username, login=False):
+    cn = connection()
+    cur = cn.cursor()
+    sql = f"SELECT 1 FROM User WHERE user_id = {id} OR username = '{username}'"
+    cur.execute(sql)
+    res = cur.fetchone()
+    if login:
+        sql = f"SELECT * FROM User WHERE user_id = {id} OR username = '{username}'"
+        cur.execute(sql)
+        res = cur.fetchone()
+    return res
+
+
+val = (11115, 'aaaa', 'bb', 'cc', 'dd', 22)
+print(check_user_exists(1, 'a', True))
+test1()
