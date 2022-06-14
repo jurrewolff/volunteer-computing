33c45f306b84bb3a995975ad5c9fef94e878c242
Tommaso Trooster <trooster.t@gmail.com>
2022-06-14 17:16:24 +0200

    Revert "Merge branch 'database' of github.com:jurrewolff/volunteer-computing into database"
    This reverts commit e43cf90b91fa31b9cf9b8a41a96b38dc1c41d1a8, reversing
    changes made to fa5d05bacbfbf246350d3f311e89595236012717.

--- app/test.py
+++ app/test.py
@@ -3,27 +3,19 @@ import models.user as user
 import models.project as project
 import models.volunteer as volunteer
 
-# print("user tests")
-# user.print_users()
-# print(user.insert_user((user.get_new_user_id(), "username2", "password", "test1@email.com", "fname", "lname", 0, 0)))
-# user.print_users()
-
-# print("\nproject tests")
-# print(project.get_all_projects())
-# project.insert_project((project.get_new_project_id(), "test_project", "this is a test project", 1, 100, 0, 100))
-# print(project.get_all_projects())
-
-# print("\nvolunteer tests")
-# print(volunteer.get_projects_of_user(1))
-# print(volunteer.insert_volunteer((1, 1, 0)))
-# print(volunteer.get_projects_of_user(1))
-
-# print("\nResearcher tests")
-# user.print_users()
-# print(user.insert_researcher((user.get_new_user_id(), "researcher1", "password", "researcher@gmail.com", "f", "l", 0, "uva", 1, "se")))
-# user.print_users()
-
-print("\ntest change password function")
+print("user tests")
 user.print_users()
-user.change_password(1, "hi")
+print(user.insert_user((user.get_new_user_id(), "username2", "password", "test1@email.com", "fname", "lname", 0, 0)))
 user.print_users()
+
+print("\nproject tests")
+print(project.get_all_projects())
+project.insert_project((project.get_new_project_id(), "test_project", "this is a test project", 1, 100, 0, 100))
+print(project.get_all_projects())
+
+print("\nvolunteer tests")
+print(volunteer.get_projects_of_user(1))
+print(volunteer.insert_volunteer((1, 1, 0)))
+print(volunteer.get_projects_of_user(1))
+
+
