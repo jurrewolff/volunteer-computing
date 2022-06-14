1d793abdaf4cfd6cf47c26706cb3df12013c4fda
Jurre Wolff <dev@jurrewolff.nl>
2022-06-14 14:49:34 +0200

    Add module as central space for flask routes. Add "/userdata" endpoint.
    Userdata endpoint currently works with a mock database. On completion of
    database functions, replace mock functions with them.

--- app/util.py
+++ app/util.py
@@ -1,8 +1,20 @@
 from http import HTTPStatus
+from flask import jsonify, Response
 
 
-def build_response(code: int, description: str = "") -> dict:
+def build_response(code: int, description: str = "", **kwargs: dict) -> Response:
+    """
+    :param code: HTTP status code
+    :param description: A custom HTTP description
+    :param kwargs: Keyword arguments, which must contain dictionaries as values
+    :return: Returns flask response ready to send back to client.
+    """
     if not description:
         description = HTTPStatus(code).description
 
-    return {"code": code, "phrase": HTTPStatus(code).phrase, "description": description}
+    data = {"code": code, "phrase": HTTPStatus(code).phrase, "description": description}
+
+    for key, value in kwargs.items():
+        data[key] = value
+
+    return jsonify(data)
