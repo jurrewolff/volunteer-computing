c6700f27b669cdbc6d5a9c78ac4a6e34aa5aa8bf
Tessa Christina van Lobbrecht <t.cvanlobbrecht@wcw-stud-145-109-29-74.wireless.uva.nl>
2022-06-22 12:11:15 +0200

    Extra save: navigation functioning -> test on server

--- flask_react/src/Pages/Login.js
+++ flask_react/src/Pages/Login.js
@@ -12,78 +12,16 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
-import { GridWrapper } from '../Components/NavbarElements';
-import Button from '@mui/material/Button';
 // import Divider from '@mui/material/Divider';
 // import Box from '@mui/material/Box';
 
 
 
-// import ResponsiveAppBar from '../Components/Navbar2';
-// import PermanentDrawerLeft from '../Components/SideMenu';
-
 export default function Login() {
     const paperStyle = { padding: 20, width: '75%' }
 
-    const [error, setError] = useState(false);
-
-
     const [fname, setFname] = useState();
     const [pass, setPass] = useState();
-
-    const [clicked, setClicked] = useState(false)
-
-    const handleClick = () => {
-        return (
-            console.log("delete")
-        )
-    }
-
-    // const normalName (props) {
-    //     return (
-    // <TextField
-    //     margin="normal"
-    //     required
-    //     id="fname"
-    //     // ref={fNameRef}
-    //     fullWidth
-    //     label="Username"
-    //     variant="outlined"
-    //     onChange={(e) => setFname(e.target.value)}
-    // />
-    //     )
-    // }
-
-    const normalName = (props) => {
-        return (
-            console.log(props["0"].msg),
-            console.log(props["0"].state)
-            // console.log(Object.keys(props))
-
-            // <TextField
-            //     margin="normal"
-            //     required
-            //     id="fname"
-            //     // ref={fNameRef}
-            //     fullWidth
-            //     label={props.msg}
-            //     variant="outlined"
-            //     onChange={(e) => setFname(e.target.value)}
-            // />
-        )
-    }
-
-
-    const errorName = (props) => {
-        return (
-            <TextField
-                error
-                id="outlined-error"
-                label="Error"
-                defaultValue="Hello World"
-            />
-        )
-    }
 
     return (
         <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
@@ -93,9 +31,16 @@ export default function Login() {
                     sx={{ textAlign: 'center' }}>Login</Typography>
                 <Grid item xs={6}>
                     <Grid >
-                        {error ? errorName([{ msg: "test" }]) : normalName([{ msg: "test1", state: "notnice" }])}
-
-
+                        <TextField
+                            margin="normal"
+                            required
+                            id="fname"
+                            // ref={fNameRef}
+                            fullWidth
+                            label="Username"
+                            variant="outlined"
+                        // onChange={(e) => setFname(e.target.value)}
+                        />
                     </Grid>
                     <Grid >
                         <TextField
@@ -106,7 +51,7 @@ export default function Login() {
                             label="Password"
                             type="password"
                             autoComplete="current-password"
-                            onChange={(e) => setPass(e.target.value)}
+                        // onChange={(e) => setPass(e.target.value)}
                         />
                     </Grid>
                     <Grid alignitems="center">
