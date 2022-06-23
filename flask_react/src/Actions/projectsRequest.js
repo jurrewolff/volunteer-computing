64d4698045d79084e58f90100b746bfade0ed727
Jan <jan.deen@student.uva.nl>
2022-06-23 14:49:49 +0200

    Change to mui

--- flask_react/src/Actions/projectsRequest.js
+++ flask_react/src/Actions/projectsRequest.js
@@ -7,69 +7,83 @@
  */
 
 import { useState, useEffect } from 'react'
-import { ListGroup, Row, Col, Button, Card} from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
 
 export const ProjectsRequest = () => {
 
     const [data, setData] = useState([{}]);
-    
+
     useEffect(() => {
             fetch("/projects")
                 .then(res => res.json())
                 .then(data => {
                     setData(data)
-                    console.log(data)
-                    console.log("now useEffect is being executed")
                 })
     }, []);
 
 
-    // Returns a card with the given project 
+    // Returns a card with the given project
     const getCard = ((project) => {
         return (
-            <Card className="mb-3" style={{width:"100%", height:"90%", marginTop:"5%", marginLeft:"15%"}}>
-                <h1>{project.name}</h1>
-                <Card.Text style={{margin:"5%"}}>{project.description}</Card.Text>
-                <Row style={{margin:"5%"}}>
-                    <Col>
-                        <Link to={"/moreInfo/" + project.project_id + "/NoRun"}>More info</Link>
-                    </Col>
-                    <Col>
-                        <Link to={"/moreInfo/" + project.project_id + "/Run"}>
-                            <Button
-                                variant="success">
-                                Start computing
-                            </Button>
-                        </Link>
-                    </Col>
-                </Row>
+            <Card style={{width:"100%", height:"90%", marginTop:"5%", marginLeft:"100px"}} sx={{ minWidth: 275 }}>
+                <CardContent>
+                    <Typography style={{margin:"5%"}} variant="h2"  gutterBottom>
+                        {project.name}
+                    </Typography >
+                    <Typography style={{margin:"5%"}} sx={{ mb: 1.5 }} color="text.secondary" variant="h5" component="div">
+                        {project.description}
+                    </Typography>
+                </CardContent>
+
+                <CardActions >
+                    <Link style={{margin:"10%"}} to={"/moreInfo/" + project.project_id + "/Run"}>
+                        <Button
+                            variant="contained" color="success">
+                            Start computing
+                        </Button>
+                    </Link>
+
+                    <Link style={{margin:"10%"}} to={"/moreInfo/" + project.project_id + "/NoRun"}>More info</Link>
+                </CardActions>
+
+
+
+
             </Card>
-        )
+          );
     });
 
     // Returns two column with all projects displayed in a card per project.
     const readProjects = ({data}) => {
-        
+
         // If no projects exists. Nothing is displayed.
-        // if(data[0] = {}) {
-        //     return <h1>no projects available</h1>
-        // }
+        if(data.length === 0) {
+            return <h1>no projects available</h1>
+        }
 
         const getRightIndex = ((index) => {
             return index + Math.ceil(data.length / 2)
         });
 
-        return data.map((project, index) => 
+        return data.map((project, index) =>
             <div>
-                
+
                 {index < Math.ceil(data.length / 2)?
                     <Row key={project.project_id}>
 
                         <Col>
                             {getCard(project)}
                         </Col>
-                        
+
                         {/* The last row should only have 1 card if there is an uneven number of projects */}
                         {index < Math.floor(data.length / 2) ?
                             <Col>
@@ -82,7 +96,7 @@ export const ProjectsRequest = () => {
             </div>
         );
     }
-    
+
     return (
         <div>
             {readProjects({data})}
