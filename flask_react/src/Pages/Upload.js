0e24c65478d8eae05e8cd4f16854820810ce62e5
Jeffrey Kragten <Jeffreykragten@live.nl>
2022-06-23 11:39:54 +0200

    Moved logic of uploading project to uploadFile.js, in Actions folder

--- flask_react/src/Pages/Upload.js
+++ flask_react/src/Pages/Upload.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import './Login.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import { UploadRequest } from '../Actions/uploadFile';
 
-import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
-import { Routes, Route, useNavigate } from "react-router-dom"
 
 export default function Upload() {
     const paperStyle = { padding: 20, margin: "20px auto" }
-	const navigate = useNavigate();
 
-    const [clicked, setClicked] = useState(false)
     const [name, setName] = useState();
     const [description, setDescription] = useState();
     const [block_size, setBlocksize] = useState();
@@ -24,35 +21,6 @@ export default function Upload() {
 	const [owner, setOwner] = useState();
     const [qorum, setQorum] = useState();
 
-	useEffect(() => {
-        if (clicked) {
-			const formData = new FormData()
-			const codeFile = document.getElementById('file').files[0]
-			const inputFile = document.getElementById('input').files[0]
-			formData.append('file', codeFile)
-			formData.append('input', inputFile)
-			console.log(codeFile)
-			console.log(inputFile)
-            fetch("/upload", {method: 'POST',
-							  headers: {'name': name,
-										'description': description,
-										'block_size': block_size,
-										'owner': owner,
-										'random_validation': random_validation,
-										'max_runtime': max_runtime,
-                                        'qorum': qorum
-										},
-							  body: formData})
-                .then((response) => response.json())
-                .then((result) => {
-                    console.log({ result })
-                });
-
-            navigate('/dashboard');
-            setClicked(false)
-        }
-	}, [clicked]);
-
     return (
         <>
             <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
@@ -60,7 +28,7 @@ export default function Upload() {
                     sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
                     <Typography component="h1" variant="h4" gutterBottom
                         sx={{ textAlign: 'center' }}>
-                        Upload Project
+                        Upload your project here
                     </Typography>
                     <Divider variant="middle" />
                     <Grid container spacing={3}>
@@ -141,12 +109,13 @@ export default function Upload() {
 								<input type="file" id="input"/>
 							</div>
 							</Grid>
-							<Button
-								variant="contained"
-								onClick={() => setClicked(true)}
-								sx={{ mt: 3, ml: 1 }}>
-								Upload Project
-							</Button>
+							<Grid alignitems="center">
+                                < UploadRequest
+                                    name={name} description={description} block_size={block_size}
+                                    owner={owner} random_validation={random_validation}
+                                    max_runtime={max_runtime} qorum={qorum}
+                                />
+                            </Grid>
                         </Grid>
                     </Grid>
                 </Paper>
