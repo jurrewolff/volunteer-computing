d183116c0ca84deb041012daa14b64670e6e284f
Lleyton Wynter <95350098+Llennox@users.noreply.github.com>
2022-06-26 21:09:58 +0200

    results

--- flask_react/src/Pages/Results.js
+++ flask_react/src/Pages/Results.js
@@ -1,283 +1,163 @@
-import React from 'react';
+import * as React from 'react';
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
 import Table from '@mui/material/Table';
-import Paper from '@mui/material/Paper';
-import Switch from '@mui/material/Switch';
-import Toolbar from '@mui/material/Toolbar';
-import Tooltip from '@mui/material/Tooltip';
-import { visuallyHidden } from '@mui/utils';
-import { alpha } from '@mui/material/styles';
-import TableRow from '@mui/material/TableRow';
-import Checkbox from '@mui/material/Checkbox';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import DeleteIcon from '@mui/icons-material/Delete';
 import TableContainer from '@mui/material/TableContainer';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import TablePagination from '@mui/material/TablePagination';
-import FormControlLabel from '@mui/material/FormControlLabel';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { makeStyles } from '@mui/styles';
+
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 import PermanentDrawerLeft from '../Components/SideMenu';
 import ResponsiveAppBar from '../Components/Navbar'
 
-function createData(title, status, runtime, date) {
+
+// const readResults = ({ data }) => {
+
+//   return data.map((project) =>
+
+
+//   );
+// }
+
+function createData(Title, Time_run, Date, Description, Project_id) {
   return {
-    title,
-    status,
-    runtime,
-    date
+    Title,
+    Time_run,
+    Date,
+    Description,
+    Project_id
   };
 }
 
-const rows = [
-  createData('test', 'done', 10, '15/02/19'),
-  createData('test1', 'done', 10, '15/02/19'),
-  createData('test2', 'done', 10, '15/02/19'),
-  createData('test3', 'done', 10, '15/02/19'),
-  createData('test4', 'done', 10, '15/02/19'),
-  createData('test5', 'pending', 10, ''),
-  createData('test6', 'done', 10, '15/02/19'),
-  createData('test7', 'done', 10, '15/02/19'),
-  createData('test8', 'done', 10, '15/02/19'),
+
+let rows = [
+  // createData('Frozen yoghurt', 159, 6.0,),
+  // createData('Ice cream sandwich', 237, 9.0, 37),
+  // createData('Eclair', 262, 16.0, 24),
+  // createData('Cupcake', 305, 3.7, 67),
+  // createData('Gingerbread', 356, 16.0, 49),
 ];
 
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+
+  const handleDownload = () => {
+    // Download request
   }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getComparator(order, orderBy) {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-// This method is created for cross-browser compatibility, if you don't
-// need to support IE11, you can use Array.prototype.sort() directly
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
-const headCells = [
-  {
-    id: 'title',
-    numeric: false,
-    disablePadding: true,
-    label: 'Project title',
-  },
-  {
-    id: 'status',
-    numeric: false,
-    disablePadding: false,
-    label: 'Status',
-  },
-  {
-    id: 'runtime',
-    numeric: true,
-    disablePadding: false,
-    label: 'Runtime (minutes)',
-  },
-  {
-    id: 'date',
-    numeric: false,
-    disablePadding: false,
-    label: 'Date',
-  },
-];
-
-function EnhancedTableHead(props) {
-  const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
-    props;
-  const createSortHandler = (property) => (event) => {
-    onRequestSort(event, property);
-  };
 
   return (
-    <TableHead>
-      <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              'aria-label': 'select all desserts',
-            }}
-          />
-        </TableCell>
-        {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? 'right' : 'left'}
-            padding={headCell.disablePadding ? 'none' : 'normal'}
-            sortDirection={orderBy === headCell.id ? order : false}
+    <React.Fragment>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
           >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </Box>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.Title}
+        </TableCell>
+        <TableCell align="left">{row.Time_run}</TableCell>
+        <TableCell align="left">{row.Date}</TableCell>
       </TableRow>
-    </TableHead>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 2 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                More info
+              </Typography>
+              <Typography>
+                {row.Description}
+              </Typography>
+              <Grid
+                container
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <Grid>
+                  <IconButton
+                    aria-label="delete"
+                    size="large"
+                    onClick={() => handleDownload(row.Project_id)}
+                  >
+                    <DeleteIcon fontSize="inherit" />
+                  </IconButton>
+                </Grid>
+                <Grid>
+                  <Button
+                    // id={row.Project_id}
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2.5, ml: 1, mr: 2 }}
+                    onClick={() => handleDownload(row.Project_id)}
+                  >
+                    Download
+                  </Button>
+                </Grid>
+              </Grid>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
   );
 }
 
-EnhancedTableHead.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-  onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-  orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
+Row.propTypes = {
+  row: PropTypes.shape({
+    Time_run: PropTypes.number.isRequired,
+    carbs: PropTypes.number.isRequired,
+    Date: PropTypes.number.isRequired,
+    history: PropTypes.arrayOf(
+      PropTypes.shape({
+        amount: PropTypes.number.isRequired,
+        customerId: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+    Title: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    protein: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
-const EnhancedTableToolbar = (props) => {
-  const { numSelected } = props;
+export default function CollapsibleTable() {
+  const [data, setData] = React.useState([{}]);
 
+  React.useEffect(() => {
+    fetch("/projects")
+      .then(res => res.json())
+      .then(data => {
+        setData(data)
+        console.log(data)
+      })
+  }, []);
+
+  const readResults = ({ data }) => {
+
+    return data.map((project) =>
+      // rows.push(createData(project.name, project.time_run, project.date, project.description, project.id))
+      console.log(project)
+    );
+  }
   return (
-    <Toolbar
-      sx={{
-        pl: { sm: 2 },
-        pr: { xs: 1, sm: 1 },
-        ...(numSelected > 0 && {
-          bgcolor: (theme) =>
-            alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
-        }),
-      }}
-    >
-      {numSelected > 0 ? (
-        <Typography
-          sx={{ flex: '1 1 100%' }}
-          color="inherit"
-          variant="subtitle1"
-          component="div"
-        >
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Typography
-          sx={{ flex: '1 1 100%' }}
-          variant="h6"
-          id="tableTitle"
-          component="div"
-        >
-          Results
-        </Typography>
-      )}
-
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton>
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton>
-            <FilterListIcon />
-          </IconButton>
-        </Tooltip>
-      )}
-    </Toolbar>
-  );
-};
-
-EnhancedTableToolbar.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-};
-
-export default function Results() {
-  const [order, setOrder] = React.useState('asc');
-  const [orderBy, setOrderBy] = React.useState('runtime');
-  const [selected, setSelected] = React.useState([]);
-  const [page, setPage] = React.useState(0);
-  const [dense, setDense] = React.useState(false);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-  const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
-    setOrderBy(property);
-  };
-
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelecteds = rows.map((n) => n.name);
-      setSelected(newSelecteds);
-      return;
-    }
-    setSelected([]);
-  };
-
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
-      );
-    }
-
-    setSelected(newSelected);
-  };
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  const handleChangeDense = (event) => {
-    setDense(event.target.checked);
-  };
-
-  const isSelected = (name) => selected.indexOf(name) !== -1;
-
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
-
-  return (
-    // <Box sx={{ width: '100%' }}>
     <>
       <ResponsiveAppBar />
       <PermanentDrawerLeft />
@@ -285,160 +165,36 @@ export default function Results() {
         border="dashed"
         component="main"
         sx={{
-          pl: 30,
+          pl: 45,
           flexGrow: 1,
           height: '100vh',
           overflow: 'auto',
         }}
       >
-        <Paper sx={{ width: '100%', mb: 2 }}>
-          <EnhancedTableToolbar numSelected={selected.length} />
-          <TableContainer>
-            <Table
-              sx={{ minWidth: 750 }}
-              aria-labelledby="tableTitle"
-              size={dense ? 'small' : 'medium'}
-            >
-              <EnhancedTableHead
-                numSelected={selected.length}
-                order={order}
-                orderBy={orderBy}
-                onSelectAllClick={handleSelectAllClick}
-                onRequestSort={handleRequestSort}
-                rowCount={rows.length}
-              />
-              <TableBody>
-                {/* if you don't need to support IE11, you can replace the `stableSort` call with:
-                 rows.slice().sort(getComparator(order, orderBy)) */}
-                {stableSort(rows, getComparator(order, orderBy))
-                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                  .map((row, index) => {
-                    const isItemSelected = isSelected(row.name);
-                    const labelId = `enhanced-table-checkbox-${index}`;
-
-                    return (
-                      <TableRow
-                        hover
-                        onClick={(event) => handleClick(event, row.name)}
-                        role="checkbox"
-                        aria-checked={isItemSelected}
-                        tabIndex={-1}
-                        key={row.name}
-                        selected={isItemSelected}
-                      >
-                        <TableCell padding="checkbox">
-                          <Checkbox
-                            color="primary"
-                            checked={isItemSelected}
-                            inputProps={{
-                              'aria-labelledby': labelId,
-                            }}
-                          />
-                        </TableCell>
-                        <TableCell
-                          component="th"
-                          id={labelId}
-                          scope="row"
-                          padding="none"
-                        >
-                          {row.name}
-                        </TableCell>
-                        <TableCell align="right">{row.calories}</TableCell>
-                        <TableCell align="right">{row.fat}</TableCell>
-                        <TableCell align="right">{row.carbs}</TableCell>
-                        <TableCell align="right">{row.protein}</TableCell>
-                      </TableRow>
-                    );
-                  })}
-                {emptyRows > 0 && (
-                  <TableRow
-                    style={{
-                      height: (dense ? 33 : 53) * emptyRows,
-                    }}
-                  >
-                    <TableCell colSpan={6} />
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TablePagination
-            rowsPerPageOptions={[5, 10, 25]}
-            component="div"
-            count={rows.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-          />
-        </Paper>
-      </Box >
+        {readResults({ data })}
+        <TableContainer component={Paper} style={{ width: '80%' }}>
+          <Table aria-label="collapsible table">
+            <TableHead>
+              <TableRow>
+                <TableCell />
+                <TableCell>Title </TableCell>
+                <TableCell align="left">Time run</TableCell>
+                <TableCell align="left">Date&nbsp;</TableCell>
+                {/* <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <Row key={row.Title} row={row} />
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
     </>
   );
 }
 
 
-// import { useState, useEffect } from 'react'
-// import { Container, Row, Col, Button, Card } from "react-bootstrap";
-// import { Link } from "react-router-dom";
 
-// import PermanentDrawerLeft from '../Components/SideMenu';
-// import ResponsiveAppBar from '../Components/Navbar'
-
-// export default function Results() {
-
-//   const [data, setData] = useState([{}]);
-
-//   useEffect(() => {
-//     fetch("/projects")
-//       .then(res => res.json())
-//       .then(data => {
-//         setData(data)
-//         console.log(data)
-//       })
-//   }, []);
-
-
-//   // Returns all projects displayed in a card per project.
-//   const readResults = ({ data }) => {
-
-//     return data.map((project) =>
-
-//       <Row key={"result" + project.project_id} className="mb-3" style={{ width: "80%", height: "100px", marginTop: "5%", marginBottom: "3%", marginLeft: "100px" }}>
-//         <Col>
-//           <Card style={{ backgroundColor: '#7BD2EC' }}>
-//             <Row>
-//               <Col style={{ margin: "3%" }}>
-//                 <h2>{project.name}</h2>
-//               </Col>
-//               <Col style={{ margin: "4%" }}>
-//                 <Link to={"/moreInfo/" + project.project_id}>More info</Link>
-//               </Col>
-//               <Col style={{ margin: "3%" }}>
-//                 Time run: 12:10:59
-//               </Col>
-//               <Col style={{ margin: "3%" }}>
-//                 Correct
-//               </Col>
-
-//             </Row>
-//           </Card>
-//         </Col>
-//         <Col xs={1} sm={1} md={1} lg={1} style={{ marginTop: "3%" }}>
-//           <Button>Download</Button>
-//         </Col>
-//       </Row>
-//     );
-//   }
-
-
-//   return (
-//     <>
-      // <ResponsiveAppBar />
-      // <PermanentDrawerLeft />
-//       <Container className="text-center" style={{ marginLeft: "5%", marginRight: "5%" }}>
-//         {readResults({ data })}
-//       </Container>
-//     </>
-//   );
-// };
