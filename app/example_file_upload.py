2e6422a2aa036cafde3ded7a61bbffa444b01a06
RSlanjouw <95342279+RSlanjouw@users.noreply.github.com>
2022-06-27 15:35:30 +0200

    Stond een line dubbel ff weggehaald

--- app/example_file_upload.py
+++ app/example_file_upload.py
@@ -233,7 +233,6 @@ def datatest(project_id):
     # TODO switch to request instead of params in url
     user_id = session["user_id"]
     if request.method == "POST":
-        job_id = request.form.get("job_id")
         data = request.form.get("data")
         job_id = request.form.get("job_id")
         receive_work(project_id, job_id, user_id, data)
