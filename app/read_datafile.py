0b4b29ffb0aa5c361e06745d089e17c3487e0a4d
Ruben Lanjouw <ruben.lanjouw14@gmail.com>
2022-07-01 13:56:09 +0200

    Needed to let cerlery work

--- app/read_datafile.py
+++ app/read_datafile.py
@@ -1,18 +1,14 @@
+"""
+Date:               29-06-2022
+Contributers:       PSE Group G
+
+File description:
+Helper functions for reading files.
+
+"""
+
 import linecache as ls
 import subprocess
-
-def file_to_arguments(filename, line=0,start_line=0,end_line=-1):
-    '''
-    Functie die een dict returned in de vorm van:
-    {'arguments': [......], size: int, lines: range(....)}
-    '''
-    if end_line == -1:
-        with open(filename, 'r') as f:
-            end_line = len(f.readlines())
-    args = list(map(str.strip, ls.getlines(filename, range(start_line, end_line))))
-    lines = [str(r) for r in range(start_line,end_line)]
-    return {'arguments': args, 'size': len(args), 'lines': lines}
-
 
 def get_line_from_file(filename, line=0):
     '''
